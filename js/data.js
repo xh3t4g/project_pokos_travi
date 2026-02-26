61c66acfd773a8650c9946d8130e1a956bab08b3
xh3t4g <xh3t4g@gmail.com>
2026-02-26 13:03:27 +0300

    удалили комантарии в файле data.js

--- js/data.js
+++ js/data.js
@@ -1,9 +1,3 @@
-// const catalogs = [
-//     {img: 'assets/img/1.jpg'},
-//     {img: 'assets/img/2.jpg'},
-//     {img: 'assets/img/3.jpg'}
-// ];
-
 const catalogs = [];
 
 for (let i = 1; i <= 3; i++) {
