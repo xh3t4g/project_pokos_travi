a7f48a4f148e43af24afa3a9b10e14543eaa504d
xh3t4g <xh3t4g@gmail.com>
2026-02-28 13:47:23 +0300

    готовая сборка с vite , можно делать build

---
+++ src/js/render.js
@@ -0,0 +1,18 @@
+const list = document.querySelector('#gallery_list');
+
+export function RenderGallery(catalogs) {
+    catalogs.forEach((catalog) => {
+        let link_a = document.createElement('a');
+        link_a.href = catalog.img;
+        link_a.rel = 'noopener';
+
+        let img_s = document.createElement('img');
+        img_s.src = catalog.img;
+        img_s.alt = '';
+
+        link_a.appendChild(img_s);
+        list.appendChild(link_a);
+        
+    })
+
+}
